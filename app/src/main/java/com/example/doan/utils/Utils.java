e8224b7879ffab4d41ebff120faa95e2429a2825
Giang2305 <146044134+Giang2305@users.noreply.github.com>
2025-05-24 13:09:19 +0700

    giohang

--- app/src/main/java/com/example/doan/utils/Utils.java
+++ app/src/main/java/com/example/doan/utils/Utils.java
@@ -2,5 +2,6 @@ package com.example.doan.utils;
 
 public class Utils {
     public static final String BASE_URL = "http://10.0.2.2:81/androidstudio_banhang/";
+    public static List<GioHang> manggiohang;
 }
 
